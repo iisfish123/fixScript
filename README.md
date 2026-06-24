c2d8dc3e7618bf10746c47a882787dd2bdbb1ad5
likezheng <nicol@fuseinsurtech.com>
2026-06-24 09:56:35 +0800

    CS-0000: 手动配置更新、文档更新

--- README.md
+++ README.md
@@ -1,19 +1,64 @@
 # fixScript
 
+## 背景
+
+vpn托管配置了`自动更新`每次自动更新后都丢失了手动配置的信息。
+为了解决上述问题，开发了这个定时器。
+
 ## ENV
+
 node14.19.0
 
 ## Usage
+
+```sh
 - npm install pm2 -g
 - npm i
+```
 
 ## 启动
-安装pm2 
-pm2 reload ecosystem.config.js --env production
 
-### 起源
-配置文件被人为手动修改后，又因自动更新的配置覆盖掉，进而丢失了手动的配置
+- 安装pm2
+
+```sh
+pm2 reload ecosystem.config.js --env production
+```
+
+### 设置开机自启动
+
+- pm2保存信息
+
+```sh
+pm2 save
+```
+
+- 设置pm2开机自启动命令
+
+```sh
+pm2 startup
+```
+
+- startup后，输出一条需要你再执行的命令，按它提示的命令继续即可。如
+
+```sh
+sudo env PATH=$PATH:/Users/xxxxx/.nvm/versions/node/v22.21.1/bin /Users/xxxxx/.nvm/versions/node/v22.21.1/lib/node_modules/pm2/bin/pm2 startup launchd -u xxxxx --hp /Users/xxxx
+```
+
+### 关闭开机自启动
+
+- pm2 unstartup：移除开机自动启动的系统启动脚本
+
+```sh
+pm2 unstartup
+```
+
+- pm2 save：更新 PM2 的保存状态，避免下次又被恢复
+
+```sh
+pm2 save
+```
 
 ### 解决
+
 - 脚本自动新增VPN配置，解决配置被覆盖问题
 - 或许有别的方法，暂时没明白
--- index.js
+++ index.js
@@ -1,33 +1,40 @@
-const path = require('path')
-const fs = require('fs')
-const JsYaml = require('js-yaml')
-const CronJob = require('cron').CronJob
+const path = require("path");
+const fs = require("fs");
+const JsYaml = require("js-yaml");
+const CronJob = require("cron").CronJob;
 
 function jobCallback() {
-  const root = path.join('/Users/lkz/.config/clash/iggfeed.yaml')
+  const root = path.join("/Users/lkz/.config/clash/iggfeed.yaml");
   // const root = path.join('./iggfeed.yaml') // dev
   try {
-    const doc = JsYaml.load(fs.readFileSync(root, 'utf8'))
-    const rules = doc.rules || []
-    if (rules.length == 0 || rules.find(item => item.includes('fuse'))) return
-    const lastIndex = rules.findIndex(item => {
-      return item.includes('XLLiveUD')
-    })
+    const doc = JsYaml.load(fs.readFileSync(root, "utf8"));
+    const rules = doc.rules || [];
+    if (rules.length == 0 || rules.find((item) => item.includes("fuse")))
+      return;
+    const lastIndex = rules.findIndex((item) => {
+      return item.includes("XLLiveUD");
+    });
     if (lastIndex > -1) {
-      rules.splice(lastIndex + 1, 0, 'DOMAIN-KEYWORD,fuse,🎯 本地直连', 'DOMAIN-KEYWORD,fuseinsurtech.com,🎯 本地直连')
+      // rules.splice(lastIndex + 1, 0, 'DOMAIN-KEYWORD,fuse,🎯 本地直连', 'DOMAIN-KEYWORD,fuseinsurtech.com,🎯 本地直连')
+      rules.splice(
+        lastIndex + 1,
+        0,
+        "DOMAIN-KEYWORD,shenzaokeji.com,🎯 本地直连",
+        "DOMAIN-KEYWORD,939394.xyz,🎯 本地直连",
+      );
     }
     // fs.writeFileSync('./test.yaml', JsYaml.dump(doc)) // dev
-    fs.writeFileSync(root, JsYaml.dump(doc))
+    fs.writeFileSync(root, JsYaml.dump(doc));
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
 }
 
 const job = new CronJob(
-  '10 * * * * *',
+  "10 * * * * *",
   jobCallback,
   null,
   true,
-  'Asia/Shanghai'
-)
-job.start()
+  "Asia/Shanghai",
+);
+job.start();
